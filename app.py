4233f114b48dc7d1070aafa3f004e34345dc600f
NikolayKalyshnuy <nikkalaba245@gmail.com>
2023-02-05 02:16:50 +0200

    Test adaptive catalog.html

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from models import MockCard
@@ -13,7 +13,16 @@ migrate = Migrate(app, db)
 
 @app.route('/')
 def catalog():
-    cards = [MockCard(1, "qwer", 123.00), MockCard(2, "asd", 456.86), MockCard(3, "zxc", 789.23)]
+    cards = [
+        MockCard(1, "qwe", 123.00, img_src=url_for('static', filename='images/mock/1.jpg')),
+        MockCard(2, "asd", 456.86, img_src=url_for('static', filename='images/mock/2.png')),
+        MockCard(3, "asd", 456.86, img_src=url_for('static', filename='images/mock/3.jpg')),
+        MockCard(4, "asd", 456.86, img_src=url_for('static', filename='images/mock/4.jpg')),
+        MockCard(5, "asd", 456.86, img_src=url_for('static', filename='images/mock/5.jpg')),
+        MockCard(6, "asd", 456.86, img_src=url_for('static', filename='images/mock/6.jpg')),
+        MockCard(7, "asd", 456.86, img_src=url_for('static', filename='images/mock/7.jpg')),
+        MockCard(8, "zxc", 789.23, img_src=url_for('static', filename='images/mock/8.png'))]
+
     return render_template("catalog.html", cards=cards)
 
 
